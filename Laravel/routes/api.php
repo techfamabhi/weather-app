90a5b132dbea808b4d39ac46d28f396dd04ff7b4
abhishekgouda <abhishekgouda882@gmail.com>
2024-08-24 17:27:28 +0530

    Remove all files from repository

--- Laravel/routes/api.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\WeatherForecastController;
-
-// Apply the 'cors' middleware
-Route::get('/weather', [WeatherForecastController::class, 'getWeather'])->middleware('cors');
